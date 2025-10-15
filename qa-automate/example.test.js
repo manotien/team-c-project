eb9b5af75d57bc63977f8f0261c623b043917ab4
LDAPHQ\sittikorn.mue <mu.sittikorn@gmail.com>
2025-10-15 10:08:32 +0700

    update image

--- qa-automate/example.test.js
+++ qa-automate/example.test.js
@@ -8,6 +8,10 @@ test('should load dashboard and click add button', async ({ page }) => {
   // Click add button
   await page.click('button:has-text("add"), button:has-text("Add"), button:has-text("ADD")');
 
+  // Click upload button and add image
+  await page.click('button:has-text("upload"), button:has-text("Upload"), button:has-text("UPLOAD")');
+  await page.setInputFiles('input[type="file"]', 'c:\\Users\\sittikorn.mue\\Documents\\team-c-project\\qa-automate\\bill.jfif');
+
   // Keep browser open after test
   await page.waitForTimeout(999999999);
 });
